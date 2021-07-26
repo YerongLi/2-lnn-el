f054791870a04d838bfd987191495bcacdade12e
Yerong <yerong.li@outlook.com>
2021-07-26 13:20:49 -0500

    normalization

--- scripts/aida/normalize.py
+++ scripts/aida/normalize.py
@@ -1,9 +1,11 @@
 import os
 import multiprocessing
+import pandas as pd
 pre = os.getenv('HOME') + '/lnn-el/data/blink_bert_box/'
 datasets = ['full_train.csv', 'full_testA.csv', 'full_testB.csv']
 datasets = [pre + d for d in datasets]
 def process(doc):
+	pd.read_csv(doc)
 	pass
 with multiprocessing.Pool(3) as p:
 	p.map(process, datasets)
