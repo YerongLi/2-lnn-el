b092813bc5ad9a45bb3ab89c179be4632134bcbc
Yerong <yerong.li@outlook.com>
2021-07-26 13:27:14 -0500

    normalization

--- scripts/aida/normalize.py
+++ scripts/aida/normalize.py
@@ -4,7 +4,7 @@ import pandas as pd
 import tqdm
 pre = os.getenv('HOME') + '/2-lnn-el/data/aida/blink_bert_box/'
 datasets = ['full_train.csv', 'full_testA.csv', 'full_testB.csv']
-datasets = [(str(i),pre + d) for i, d in enumerate(datasets)]
+datasets = [(i,pre + d) for i, d in enumerate(datasets)]
 def process(doc):
 	i, doc = doc
 	df = pd.read_csv(doc)
