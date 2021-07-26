f8939ba67db08ee17ca29cc7d488f3d87f8040c9
Yerong <yerong.li@outlook.com>
2021-07-26 13:24:59 -0500

    normalization

--- scripts/aida/normalize.py
+++ scripts/aida/normalize.py
@@ -1,11 +1,14 @@
 import os
 import multiprocessing
 import pandas as pd
+import tqdm
 pre = os.getenv('HOME') + '/2-lnn-el/data/aida/blink_bert_box/'
 datasets = ['full_train.csv', 'full_testA.csv', 'full_testB.csv']
 datasets = [str(i) + ';' + pre + d for i, d in enumerate(datasets)]
 def process(doc):
-	pd.read_csv(doc)
-	pass
+	i, doc = doc.split(';')
+	df = pd.read_csv(doc)
+	for _ in tqdm.tqdm(range(df.shape[0]), position=i):
+		pass
 with multiprocessing.Pool(3) as p:
 	p.map(process, datasets)
